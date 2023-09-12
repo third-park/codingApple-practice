78d6d5830b1f3f81a53b41f0416f4653efe410a2
thrid-park <qkrtpaud01@gmail.com>
2023-09-12 16:53:16 +0900

    유효성체크, 3의배수체크

--- Level1/js/app.js
+++ Level1/js/app.js
@@ -1,13 +1,33 @@
-// @import { getNode, getNodes } from "../../lib/index.js";
+import { addClass, disableElement, getNode, getNodes, removeClass, showAlert} from "../../lib/index.js";
 
-const loginBtn = document.querySelector('.login-btn');
-const modalwindow = document.querySelector('.white-bg');
-const closeBtn = document.querySelector('#close');
+
+const loginBtn = getNode('.login-btn');
+const modalwindow = getNode('.white-bg');
+const closeBtn = getNode('#close');
+const submitBtn = getNode('.submit-btn');
 
 loginBtn.addEventListener('click', ()=>{
-  modalwindow.classList.add('active');
+  addClass(modalwindow, 'active');
+})
+closeBtn.addEventListener('click',()=>{
+  removeClass(modalwindow, 'active');
 })
 
-closeBtn.addEventListener('click',()=>{
-  modalwindow.classList.remove('active');
+//데이터 전송시 공백 검사
+function checkForm(){
+  const inputs = getNodes('input');
+  const [id, pw] = [...inputs];
+  let idText = id.value;
+  let pwText = pw.value;
+  if(idText === '' || pwText === ''){
+    event.preventDefault();
+    showAlert('.alert', '빈칸이다')
+  }else if(pwText.length < 6){
+    event.preventDefault();
+    showAlert('.alert', '비밀번호는 6자리 이상으로 작성', 1500)
+  }
+}
+
+submitBtn.addEventListener('click',()=>{
+  checkForm();
 })
