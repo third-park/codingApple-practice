f4722dbdd4e124233f2d29c847e9a138cf81c17f
thrid-park <qkrtpaud01@gmail.com>
2023-09-14 15:59:48 +0900

    수정, 커피총량 구하기, setTimeout이용 연습

--- Level2/js/app.js
+++ Level2/js/app.js
@@ -2,43 +2,69 @@ let 예금 = 60000;
 let 예금2 = 50000;
 const 이율 = 20;
 
-function calcMoney(예금, 예치기간){
-  if(예금 >= 50000){
-    let 총액 = 예금 * 예치기간 * 12 * 1.20;
-    console.log('이율',예금 * 12 * 예치기간 / 5)
-    console.log('총액', 총액);
-  }else if(예금 < 50000){
+function calcMoney(예금, 예치기간) {
+  if (예금 >= 50000) {
+    let 총액 = 예금 * 예치기간 * 12 * 1.2;
+    console.log("이율", (예금 * 12 * 예치기간) / 5);
+    console.log("총액", 총액);
+  } else if (예금 < 50000) {
     let 총액 = 예금 * 예치기간 * 12 * 1.15;
-    console.log('이율', 예금 * 12 * 예치기간 * 0.15)
-    console.log('총액', 총액);
+    console.log("이율", 예금 * 12 * 예치기간 * 0.15);
+    console.log("총액", 총액);
   }
 }
 
-const 계산 = document.querySelector('.calc');
-const 입력 = document.querySelector('.calc>input');
+const 계산 = document.querySelector(".calc");
+const 입력 = document.querySelector(".calc>input");
 const [풋, 금, 치, 총액] = [...계산.children];
 
-입력.addEventListener('change',function(){
-
+입력.addEventListener("change", function () {
   calcMoney(this.value, 2);
-})
+});
 
+const coffeeVolume = document.querySelector(".coffee-volume");
+const text = document.querySelector(".coffee");
+const totalCoffee = document.querySelector(".total");
 
-const coffeeVolume = document.querySelector('.coffee-volume');
-const text = document.querySelector('.coffee');
-const totalCoffee = document.querySelector('.total');
+function coffeeShare(volume) {
+  let second = (volume / 3) * 2;
+  let third = (second / 3) * 2;
 
-function coffeeShare(volume){
-  // 첫커피 -> 첫커피 / 3 * 2  -> 의 / 3 * 2
-  volume / 3 * 2 
+  let total = volume + second + third;
 
-
-  
+  if (typeof volume !== "number" || typeof total !== "number") {
+    console.error("숫자형이 아닙니다.");
+  } else {
+    text.textContent = `${volume}`;
+    totalCoffee.textContent = `${parseInt(total)}`;
+  }
 }
 
+coffeeVolume.addEventListener("change", function () {
+  coffeeShare(+coffeeVolume.value);
+});
 
-coffeeVolume.addEventListener('change', function(){
+const modal = document.querySelector(".modal");
+const [button, modalBox] = [...modal.children];
 
-  coffeeShare();
-})
+let count = 5;
+let second;
 
+button.addEventListener("click", () => {
+  modalBox.style.display = "block";
+
+  clearInterval(second);
+  count = 5;
+
+  second = setInterval(interval, 1000);
+  setTimeout(timeout, 5000);
+});
+
+function interval(){
+  count -= 1;
+  count >= 0 ?  modalBox.children[0].textContent = count : modalBox.children[0].textContent = 5;
+}
+
+function timeout(){
+  modalBox.style.display = "none";
+}
