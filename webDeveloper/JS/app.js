79ec54796271f3e333f775497b62e4ac6cb54064
thrid-park <qkrtpaud01@gmail.com>
2023-11-17 17:06:45 +0900

    랜덤 배경

--- webDeveloper/JS/app.js
+++ webDeveloper/JS/app.js
@@ -45,11 +45,12 @@ const fullName = ({ firstName, lastName }) => `${firstName} ${lastName}`;
 
 
 //! rainbow
-const colors = ['red','orange','yellow','green', 'blue', 'navy', 'violet'];
+const colors = ['red','orange','yellow','green', 'blue', 'navy', 'purple'];
 const words = document.querySelectorAll('h1>span')
 const h1 = document.querySelector('h1');
 const LowerBtn = document.createElement('button');
 const UpperBtn = document.createElement('button');
+const colorBtn = document.createElement('button');
 
 let count = 0;
 for(let text of words){
@@ -72,10 +73,46 @@ const changeUpperCase = () => changeText(e => e.toUpperCase());
 
 LowerBtn.append('소문자');
 UpperBtn.append('대문자');
+colorBtn.append('색바꾸기');
 
 h1.insertAdjacentElement('beforeend', LowerBtn);
 h1.insertAdjacentElement('beforeend', UpperBtn);
+h1.insertAdjacentElement('beforeend', colorBtn);
 
 LowerBtn.addEventListener('click', changeLowerCase);
 UpperBtn.addEventListener('click', changeUpperCase);
 
+//! randomBackgroundColor
+const body = document.body;
+const div = document.createElement('div');
+
+h1.insertAdjacentElement('beforeend', div)
+// div.append('rgb:')
+
+function randomColor(){
+  const r = Math.floor(Math.random() * 255);
+  const g = Math.floor(Math.random() * 255);
+  const b = Math.floor(Math.random() * 255);
+  return `rgb(${r},${g},${b})`
+}
+colorBtn.addEventListener('click', ()=>{
+  document.body.style.backgroundColor = randomColor();
+  div.textContent = body.style.backgroundColor;
+
+  //rgb 각 자리의 합이 100 이하면 흰색으로 바뀌게 해야한다 ㅡㅡ;
+  let bgcArray = body.style.backgroundColor;
+  getNum(bgcArray).length -2 < 6 ? div.style.color = `#FFF` : div.style.color = '';
+  
+});
+
+
+//숫자만
+function getNum(str){
+  let re = /\s*(?:;|$)\s*/;
+  let result = '';
+  str.split(re)
+  for(let e of str){
+    if(!isNaN(e)) result += e
+  }
+  return result.trim()
+}
